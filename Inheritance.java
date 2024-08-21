4d1ffcba3eb0e72f6b243ca5bc74b78e1a8e8ffe
SagarSharma950 <156985687+SagarSharma950@users.noreply.github.com>
2024-08-21 10:51:38 +0530

    Update Inheritance.java

--- Inheritance.java
+++ Inheritance.java
@@ -1,10 +1,10 @@
-class Inheritance{  
+class Programmer{  
  float salary=40000;  
 }  
-class Programmer extends Inheritance{  
+class Inheritance extends Programmer{  
  int bonus=10000;  
  public static void main(String args[]){  
-   Programmer p=new Programmer();  
+   Inheritance p=new Inheritance();  
    System.out.println("Programmer salary is:"+p.salary);  
    System.out.println("Bonus of Programmer is:"+p.bonus);  
 }  
